9f9819dede57d036efc69433777f3e520dade5b4
Sander <86840128+Sander2222@users.noreply.github.com>
2023-09-28 10:04:16 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,8 +1,36 @@
 --Made by Sander#2211
 
-Config                            = {}
+Config = {}
+Config.Price = 1000
 
-Config.Coords                     =        {x= 299.75329589844, y= -576.01910400391, z= 42.260848999023} 
-Config.Price                      = 1000 
+Config.Blip = {
+    ID = 61,
+    Scale = 1.0,
+    Color = 1,
+    Display = 4,
+    Name = 'Revive-Station',
+}
 
-Config.Blipname                   = 'Revive-Station'
+Config.Coords = {
+    vector3(123, 123, 123),
+    vector3(123, 123, 123)
+}
+
+Config.Locals = {
+    PressE = 'Press ~INPUT_CONTEXT~ to use the Revive-Station',
+    NoPerson = 'There is no person near you',
+    Revived = 'You revived: %s',
+    GotRevived = 'You got revived from: %s',
+    NotEnoughMoney = 'You dont have enough money with you',
+    NotDead = 'The player is not dead',
+    IdNotExist = 'The ID doesnt exists',
+}
+
+Config.ClientNotify = function(msg)
+    ESX.ShowNotification(msg)
+end
+
+Config.ServerNotify = function (msg, player)
+    local xPlayer = ESX.GetPlayerFromId(player)
+    xPlayer.showNotification(msg)
+end
