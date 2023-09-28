c5ea184cb396869adbba1defab4b259df6aaad71
Sander <86840128+Sander2222@users.noreply.github.com>
2023-09-28 10:04:48 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,12 +1,6 @@
 ESX = exports["es_extended"]:getSharedObject()
 local display = false
 
-
---Debug
---[[RegisterCommand("keybinds", function(source, args)
-    SetDisplay(not display)
-end)]]--
-
 RegisterNUICallback("exit", function(data)
     SetDisplay(false)
 end)
@@ -18,13 +12,12 @@ RegisterNUICallback("register", function(data, cb)
     local health = GetEntityHealth(GetPlayerPed(closestPlayer))
     
     if closestPlayer == -1 or closestDistance > 3.0 then
-        TriggerEvent('esx:showAdvancedNotification', 'Revive-Station', 'Person', 'There is no person near you.', 'CHAR_CALL911', 1)
+        Config.ClientNotify(Config.Locals.NoPerson)
     else 
 
     TriggerServerEvent('sa_revive:Revive', PlayerID, GetPlayerServerId(closestPlayer), health)
     end
 end)
-
 
 function SetDisplay(bool)
     display = bool
@@ -39,52 +32,37 @@ end
 CreateThread(function()
     while true do
         Wait(0)
-        for key, value in pairs(Config.Coords) do
-            DrawMarker(20, vector3(Config.Coords.x, Config.Coords.y, Config.Coords.z + 1), 0.0, 0.0, 0.0, 0, 0.0, 0.0, 1.0, 1.0, 1.0, 255, 0, 0, 100, false, true, 2, true, false, false, false)
-        end
-    end
-end)
-
-CreateThread(function()
-    while true do
-        Wait(0)
-
-        for key, value in pairs(Config.Coords) do
-            local dist = GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), vector3(Config.Coords.x, Config.Coords.y, Config.Coords.z))
+        for _, coords in ipairs(Config.Coords) do
+            local dist = #(GetEntityCoords(PlayerPedId()) - coords)
 
             if dist <= 2.0 then
-                ESX.ShowHelpNotification("Press ~INPUT_CONTEXT~ to use the Revive-Station")
+                ESX.ShowHelpNotification(Config.Locals.PressE)
 
                 if IsControlJustReleased(0, 38) then
                     SetDisplay(true)
                 end
+            elseif dist <= 5.0 then
+                DrawMarker(20, vector3(coords.x, coords.y, coords.z + 1), 0.0, 0.0, 0.0, 0, 0.0, 0.0, 1.0, 1.0, 1.0, 255, 0, 0, 100, false, true, 2, true, false, false, false)
+
+            else
+                Wait(1000)
             end
         end
     end
 end)
 
 CreateThread(function()
-    for _, coords in pairs(Config.Coords) do
-        local blip = AddBlipForCoord(vector3(Config.Coords.x, Config.Coords.y, Config.Coords.z))
+    for k, v in pairs(Config.Coords) do
+        local blip = AddBlipForCoord(v)
 
-        SetBlipSprite(blip, 61)
-        SetBlipScale(blip, 1.0)
-        SetBlipColour(blip, 1)
-        SetBlipDisplay(blip, 4)
+        SetBlipSprite(blip, Config.Blip.ID)
+        SetBlipScale(blip, Config.Blip.Scale)
+        SetBlipColour(blip, Config.Blip.Color)
+        SetBlipDisplay(blip, Config.Blip.Display)
         SetBlipAsShortRange(blip, true)
 
         BeginTextCommandSetBlipName("STRING")
-        AddTextComponentString(Config.Blipname)
+        AddTextComponentString(Config.Blip.Name)
         EndTextCommandSetBlipName(blip)
-        end
+    end
 end)
-
---Serverside Notify
-RegisterNetEvent('sa_revive:showNotify')
-AddEventHandler('sa_revive:showNotify', function(title, subtitle, text, pic)
-    Test()
-end)
-
-function Test(lol)
-    ESX.ShowHelpNotification('ff')
-end
