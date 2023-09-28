7ccd6a5f8c9792eff5680d9aec223f1c09111184
Sander <86840128+Sander2222@users.noreply.github.com>
2023-09-28 10:03:14 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,32 +1,26 @@
---made by Sander#2211
-
 ESX = exports["es_extended"]:getSharedObject()
 
 RegisterServerEvent('sa_revive:Revive')
 AddEventHandler('sa_revive:Revive', function(id, target, health)
     local _source = source
-	local xPlayer = ESX.GetPlayerFromId(_source)
+    local xPlayer = ESX.GetPlayerFromId(_source)
     local MoneyX  = xPlayer.getMoney()
-	local xTarget = ESX.GetPlayerFromId(target)
-
-
+    local xTarget = ESX.GetPlayerFromId(target)
 
     if tonumber(id) == tonumber(target) then
         if health < 1 then
             if MoneyX >  Config.Price then
                 xPlayer.removeAccountMoney('money', Config.Price)
-                TriggerClientEvent('sa_revive:showNotify', _source, 'Revive-station', 'Revive','you revived: ~g~' ..xTarget.getName().. '~w~',  'CHAR_CALL911')
-                TriggerClientEvent('sa_revive:showNotify', target, 'Revive-station', 'Revive','you would be revived from: ~g~' ..xPlayer.getName().. '~w~',  'CHAR_CALL911')
+                Config.ServerNotify( (Config.Locals.Revived):format(xTarget.getName()), _source)
+                Config.ServerNotify((Config.Locals.GotRevived):format(xPlayer.getName()),  target)
                 xTarget.triggerEvent('esx_ambulancejob:revive')
             else
-                TriggerClientEvent('sa_revive:showNotify', _source, 'Revive-station', 'Money','You dont have enough money with you',  'CHAR_CALL911')
+                Config.ServerNotify(Config.Locals.NotEnoughMoney,  _source)
             end 
         else 
-            TriggerClientEvent('sa_revive:showNotify', _source, 'Revive-station', 'Player','The player is not dead',  'CHAR_CALL911')
+            Config.ServerNotify(Config.Locals.NotDead, _source)
         end
     else
-        TriggerClientEvent('sa_revive:showNotify', _source, 'Revive-station', 'Player','The ID doesnt exists',  'CHAR_CALL911')
+        Config.ServerNotify(Config.Locals.IdNotExist, _source)
     end
 end)
-
-
