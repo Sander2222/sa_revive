4fe7f9489424ce3e457c8080cc2480702bfd940d
Sander <heinzwalter0987@gmail.com>
2023-10-07 03:26:34 +0200

    Create svconfig.lua

---
+++ svconfig.lua
@@ -0,0 +1,9 @@
+SVConfig = {}
+
+SVConfig.Webhook = {
+    Link = 'Webhook',
+    Color = 123,
+    Title = 'Revive Station',
+    Footer = 'Muba der Boss',
+    Message = 'Der Spieler %s (%s, %s) hat einen anderen Spieler revived %s (%s, %s)',
+}
